b8198c9bdf72a2a7421a2a8749a3954b85d9969a
merampe0892 <moi.orionix92@proton.me>
2026-09-15 21:50:44 -0500

    added the start of the routing

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -100,7 +100,7 @@ const NewItems = () => {
             </div>
           </div>
 
-          <Link to="/item-details">
+          <Link to={`/item-details/${item.id}`}>
             <img
               src={item.nftImage}
               className="lazy nft__item_preview"
